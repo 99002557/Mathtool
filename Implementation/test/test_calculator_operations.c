077c5b36304822eab2d80561ca6b8856dba9bb15
99002557 <71481173+99002557@users.noreply.github.com>
2020-09-19 02:37:30 +0530

    Update test_calculator_operations.c

--- Implementation/test/test_calculator_operations.c
+++ Implementation/test/test_calculator_operations.c
@@ -13,9 +13,9 @@ void test_divide(void);
 void test_lcm(void);
 void test_gcd(void);
 /*void test_permutation(void);
-void test_combination(void);
+void test_combination(void);*/
 void test_factorial(void);
-void test_powerof(void);*/
+void test_powerof(void);
 
 /* Start of the application test */
 int main() {
@@ -34,8 +34,8 @@ int main() {
   CU_add_test(suite, "divide", test_divide);
   CU_add_test(suite, "lcm", test_lcm);
    CU_add_test(suite, "gcd", test_gcd);
-   // CU_add_test(suite, "permutation", test_permutation);
- // CU_add_test(suite, "combination", test_combination);
+    CU_add_test(suite, "permutation", test_permutation);
+ CU_add_test(suite, "combination", test_combination);
 //CU_add_test(suite, "factorial", test_factorial);
 //CU_add_test(suite, "powerof", test_powerof);
 
@@ -96,12 +96,12 @@ void test_gcd(void) {
   CU_ASSERT(2 == permutation(2,1));  
 }
 void test_combination(void) {
- CU_ASSERT(3 == combination(3,2));  
+ CU_ASSERT(3 == combination(3,2));  */
 }
 void test_factorial(void) {
   CU_ASSERT( 24== factorial(4));  
 }
 void test_powerof(void) {
   CU_ASSERT(27 == powerof(3,3));  
-}*/
+}
 
